599223b61d66954fd40a51b2c88cd6485fd8a343
pwambui93@gmail.com <pwambui93@gmail.com>
2021-05-26 17:10:50 +0300

    Add functions for the logic

---
+++ main.js
@@ -0,0 +1,18 @@
+// == dom manupilation
+
+const OpenClose = document.getElementById('open_close');
+
+function Close() {
+
+};
+
+function Open() {
+
+};
+
+
+// === OnClick function
+
+OpenClose.addEventListener('click', () => {
+   console.log('clicked')
+})
